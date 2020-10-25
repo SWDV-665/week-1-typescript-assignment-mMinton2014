8d36133b318b1597793166eedc97a7cd78fb3d3f
mMinton2014 <56810792+mMinton2014@users.noreply.github.com>
2020-10-25 11:12:02 -0500

    added files to submit for wk1 assignment

---
+++ grocery.ts
@@ -0,0 +1,37 @@
+//Melinda Minton
+//Week 1 - SWDV 665 - Advance Software Topics 
+//October 25, 2020
+
+// create a class
+class Grocery {
+    // declare properties and corresponding data type
+    name: string;
+    quantity: number;
+    price: number;
+
+    // add a constructor
+    constructor(n: string, q: number, p: number){
+        this.name = n;
+        this.quantity = q;
+        this.price = p;
+    }
+}
+
+// create a list of grocery items
+let list_of_items = [
+    new Grocery("milk", 3, 10),
+    new Grocery("bread", 6, 25),
+    new Grocery("egg", 11, 10),
+    new Grocery("Apple",2, 1)
+]
+
+// access the html element with app id
+const ele = document.getElementById("app");
+
+// create a <p> element for each item in list and
+// append it to the html page
+list_of_items.forEach(e => {
+    const p = document.createElement("p");
+    p.textContent = `${e.name} ${e.quantity} -> $${e.price}`;
+    ele.appendChild(p);
+});
